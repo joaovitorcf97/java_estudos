4636a474f2d9370a5966787feac21962293d9a52
Joao Vitor <joaovitorcfdev@gmail.com>
2022-06-27 19:13:56 -0300

    primeiro codigo, declaracao de variaveis e operado IF e ELSE

---
+++ src/basico/DeclaracaoDeVariaveis.java
@@ -0,0 +1,20 @@
+package basico;
+
+/**
+ * byte
+ * short
+ * int
+ * long
+ * float
+ * char
+ * boolean
+ */
+
+public class DeclaracaoDeVariaveis {
+    public static void main(String[] args) {
+        int tamanho = 7;
+        double salario = 7000.25;
+        char inicial = 'J';
+        boolean ehHomem = true;
+    }
+}
---
+++ src/basico/PrimeiroPrograma.java
@@ -0,0 +1,7 @@
+package basico;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Alô mundo!!");
+    }
+}

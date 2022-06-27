8de7d3f81a7dfa10e0fedec6782e8722daa5d1b0
Joao Vitor <joaovitorcfdev@gmail.com>
2022-06-27 19:19:01 -0300

    operador ternario

---
+++ src/operadores/OperadorTernario.java
@@ -0,0 +1,8 @@
+package operadores;
+
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int idade = 17;
+        System.out.println((idade >= 18) ? "Maior de idade" : "Menor de idade");
+    }
+}

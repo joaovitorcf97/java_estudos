dcb6cf7ca487d18a4b13463230d392b9aa1c95a1
Joao Vitor <joaovitorcfdev@gmail.com>
2022-08-09 20:34:10 -0300

    polimorfismo

---
+++ src/OOP/herenca/Escola.java
@@ -0,0 +1,7 @@
+package OOP.herenca;
+
+public class Escola {
+    public void cadastrarPessoa(Pessoa pessoa) {
+        // fazer alguma coisa
+    }
+}
--- src/OOP/herenca/Main.java
+++ src/OOP/herenca/Main.java
@@ -2,10 +2,14 @@ package OOP.herenca;
 
 public class Main {
     public static void main(String[] args) {
-        Professor professor = new Professor("Pedro", 40, 10000);
+        Pessoa professor = new Professor("Pedro", 40, 10000);
         professor.fazerAniversario();
 
-        Aluno aluno = new Aluno("João", 11, "5º ano");
+        Pessoa aluno = new Aluno("João", 11, "5º ano");
         aluno.fazerAniversario();
+
+        Escola escola = new Escola();
+        escola.cadastrarPessoa(professor);
+        escola.cadastrarPessoa(aluno);
     }
 }
